1053c5bc862c09b7f57a4aebf4671cf89787922f
Felix Sehnert <felixsehnert@yahoo.de>
2024-02-17 13:01:24 +0100

    - added missing tests

--- internal/application/application_test.go
+++ internal/application/application_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Hydoc/goo/internal/view"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -82,6 +83,232 @@ func Test_Main(t *testing.T) {
 		Main(v, "./")
 
 		want := "ID  TASK         STATUS\n-----------------------\n1   Hello World    ○\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("with delete flag", func(t *testing.T) {
+		tearDown := setUpFile(t, "./.goo.json", []*model.Todo{
+			model.NewTodo("should be deleted", 1),
+		})
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		defer tearDown()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		flag.CommandLine = flag.NewFlagSet("with delete flag", flag.ExitOnError)
+		os.Args = []string{"with delete flag", "-d", "1"}
+		Main(v, "./")
+
+		want := "ID  TASK      STATUS\n--------------------\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("with toggle flag", func(t *testing.T) {
+		tearDown := setUpFile(t, "./.goo.json", []*model.Todo{
+			model.NewTodo("should be toggled", 1),
+		})
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		defer tearDown()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		flag.CommandLine = flag.NewFlagSet("with toggle flag", flag.ExitOnError)
+		os.Args = []string{"with toggle flag", "-t", "1"}
+		Main(v, "./")
+
+		want := "ID  TASK               STATUS\n-----------------------------\n\x1b[90m1   should be toggled    ✓\x1b[0m\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("with edit flag", func(t *testing.T) {
+		tearDown := setUpFile(t, "./.goo.json", []*model.Todo{
+			model.NewTodo("should be changed", 1),
+		})
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		defer tearDown()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		flag.CommandLine = flag.NewFlagSet("with edit flag", flag.ExitOnError)
+		os.Args = []string{"with edit flag", "-e", "1 Hello there!"}
+		Main(v, "./")
+
+		want := "ID  TASK          STATUS\n------------------------\n1   Hello there!    ○\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("with clear flag", func(t *testing.T) {
+		tearDown := setUpFile(t, "./.goo.json", []*model.Todo{
+			model.NewTodo("should be deleted", 1),
+			model.NewTodo("should also be deleted", 2),
+		})
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		defer tearDown()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		flag.CommandLine = flag.NewFlagSet("with clear flag", flag.ExitOnError)
+		os.Args = []string{"with clear flag", "-clear"}
+		Main(v, "./")
+
+		want := "ID  TASK      STATUS\n--------------------\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("with list flag", func(t *testing.T) {
+		tearDown := setUpFile(t, "./.goo.json", []*model.Todo{
+			model.NewTodo("Hi", 1),
+		})
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		defer tearDown()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		flag.CommandLine = flag.NewFlagSet("with list flag", flag.ExitOnError)
+		os.Args = []string{"with list flag", "-l"}
+		Main(v, "./")
+
+		want := "ID  TASK    STATUS\n------------------\n1   Hi        ○\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("with file flag", func(t *testing.T) {
+		fileToUse := "another.json"
+		tearDownFile := setUpFile(t, fileToUse, []*model.Todo{
+			model.NewTodo("I should be printed", 1),
+		})
+		defer tearDownFile()
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		flag.CommandLine = flag.NewFlagSet("with file flag", flag.ExitOnError)
+		os.Args = []string{"with file flag", "-f", fileToUse, "-l"}
+		Main(v, "./")
+
+		want := "ID  TASK                 STATUS\n-------------------------------\n1   I should be printed    ○\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("print error when file creating fails", func(t *testing.T) {
+		fileToUse := "/my-file.json"
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		os.Args = []string{"print error when file creating fails", "-f", fileToUse}
+		flag.CommandLine = flag.NewFlagSet("print error when file creating fails", flag.ExitOnError)
+		Main(v, "")
+
+		want := "open /my-file.json: permission denied\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("correct flag.Usage", func(t *testing.T) {
+		tearDown := setUpFile(t, "./.goo.json", make([]*model.Todo, 0))
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		defer tearDown()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		os.Args = []string{"correct flag.Usage"}
+		flag.CommandLine = flag.NewFlagSet("correct flag.Usage", flag.ExitOnError)
+		Main(v, "./")
+		flag.Usage()
+
+		want := "How to use goo\n  -h, --help\n    \tPrints this information\n\n  -f, --file\n    \tPath to a file to use (has to be json, if the file does not exist it gets created)\n\n  -l, --list\n        List all todos\n\n  -t, --toggle\n        Toggle the state of a todo by its id\n\n  -d, --delete\n        Delete a todo by its id\n\n  -e, --edit\n        Edit a todo by its id and a new label, use '{}' to insert the old value\n             e.g goo --edit 1 {} World!\n\n  -a, --add\n        Add a new todo\n\n  --clear\n        Clear the whole list\n"
+
+		if !strings.Contains(buffer.String(), want) {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("todolist creating fails", func(t *testing.T) {
+		file := "./.goo.json"
+		tearDown := setUpFile(t, file, map[string]int{})
+		defer tearDown()
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		os.Args = []string{"todolist creating fails", "-f", file}
+		flag.CommandLine = flag.NewFlagSet("todolist creating fails", flag.ExitOnError)
+		Main(v, "./")
+
+		want := "json: cannot unmarshal object into Go value of type []*model.Todo\n"
+
+		if buffer.String() != want {
+			t.Errorf("want %#v, got %#v", want, buffer.String())
+		}
+	})
+
+	t.Run("print error when controller returns error", func(t *testing.T) {
+		file := "./.goo.json"
+		tearDown := setUpFile(t, file, make([]*model.Todo, 0))
+		defer tearDown()
+		oldArgs := os.Args
+		defer func() {
+			os.Args = oldArgs
+		}()
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		v := view.New(buffer)
+
+		os.Args = []string{"todolist creating fails", "-a"}
+		flag.CommandLine = flag.NewFlagSet("todolist creating fails", flag.ExitOnError)
+		Main(v, "./")
+
+		want := "empty todo is not allowed\n"
 
 		if buffer.String() != want {
 			t.Errorf("want %#v, got %#v", want, buffer.String())
